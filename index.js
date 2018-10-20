c85f42ac7c0e52e7eae51f5b6d261984c858cfc9
Amit Joseph <amitbj96@yahoo.com>
2018-10-20 08:45:00 +0530

    Added URI

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 //Get the external modules
 var request = require('request');
 var cheerio = require('cheerio');
+
+//All the uri that way2sms uses
+const uri_login = "http://www.way2sms.com/re-login";
+const uri_token="http://www.way2sms.com/send-sms";
+const uri_smstoss="http://www.way2sms.com/smstoss";
